73b2dfe7c10148fcb53f0e928ca012035dcc0cbc
leovnoliveira <consultoria1.ceo@gmail.com>
2025-03-17 20:39:47 -0300

    Avoiding hot-reload on dashboard debug

--- src/dash_py_mercado_financeiro/formato.py
+++ src/dash_py_mercado_financeiro/formato.py
@@ -57,7 +57,12 @@ app.layout = dbc.Container(
 
 
 if __name__ == "__main__":
-    app.run_server(debug = False, host = "0.0.0.0", port = 8051)
+    app.run_server(debug = False,
+                   dev_tools_hot_reload = False,
+                   dev_tools_ui = False,
+                   dev_tools_props_check = False,
+                   host = "0.0.0.0",
+                   port = 8051)
 
     
 
