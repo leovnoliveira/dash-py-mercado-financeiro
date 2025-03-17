d174edc29db5577b6f863896ef2c5100b77da475
leovnoliveira <consultoria1.ceo@gmail.com>
2025-03-17 09:12:33 -0400

    Alterando o debug do dash para carregar mais rapido

--- src/dash_py_mercado_financeiro/formato.py
+++ src/dash_py_mercado_financeiro/formato.py
@@ -57,7 +57,7 @@ app.layout = dbc.Container(
 
 
 if __name__ == "__main__":
-    app.run_server(debug = True, host = "0.0.0.0", port = 8051)
+    app.run_server(debug = False, host = "0.0.0.0", port = 8051)
 
 
 
