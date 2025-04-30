e282eab4fa444618484991859fa2d082ca206a61
leovnoliveira <consultoria1.ceo@gmail.com>
2025-04-29 23:03:16 -0300

    Refactor data files and update dependencies
    - Removed outdated entries from dolar.csv and added new USD values for April 2025.
    - Updated inflacao.csv to include April 2025 inflation data.
    - Added new articles to noticias.csv, including recent economic and technology news.
    - Updated tickers.csv to include new stock tickers (VTRU3 and PETZ3) and removed DASA11.
    - Upgraded Django to version 5.2 and jinja2 to version 3.1.6 in poetry.lock.
    - Refactored web scraping logic in dados_di.py for improved readability and error handling.
    - Deleted unused test files: __init__.py and dados_noticias_web_bj.py.

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-import MetaTrader5 as mt5
-
-
-mt5.symbol_select("IVVB11", True)  # Exemplo
-info = mt5.symbol_select("IVVB11")
-if info is None:
-    print("Ainda não foi encontrado")
-else:
-    print("Sim, foi encontrado")
